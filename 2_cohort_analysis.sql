ec4e5f364d08b27fd2f68aa558a88dbdfcaae066
Arindam Sharma <arindamsharma470@gmail.com>
2026-01-21 22:50:49 +0530

    first commit

---
+++ 2_cohort_analysis.sql
@@ -0,0 +1,11 @@
+SELECT 
+	cohort_year,
+	COUNT(DISTINCT customerkey) AS total_customers,
+	ROUND(SUM(total_net_revenue)::NUMERIC, 2) AS total_revenue,
+	ROUND((SUM(total_net_revenue) / COUNT(DISTINCT customerkey))::NUMERIC, 2) AS customer_revenue
+FROM 
+	cohort_analysis 
+WHERE
+	orderdate = first_purchase_date 
+GROUP BY 
+	cohort_year
